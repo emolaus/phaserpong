21475f35abe38b4e3207e796d357178197f4210a
emolaus <bakaren@gmail.com>
2015-06-06 21:55:18 +0000

    v0.3: Items 10 och 12 klara, objekt ServerTalk på klientsidan.

---
+++ scripts/servertalk.js
@@ -0,0 +1,30 @@
+function ServerTalk() {
+    this.socket = null;
+    this.IP = '188.226.175.184';
+    this.dataCallback = null;
+}
+ServerTalk.prototype.init = function (io, usessh) {
+    if (usessh)
+        this.socket = io('https://' + this.IP, {secure: true});
+    else 
+        this.socket = io('http://' + this.IP);
+    console.log(this.socket);
+    
+    /*this.socket.emit('register');
+    this.socket.on('registered', function (msg) {
+        if (msg.host)
+            console.log('You are host');
+        else 
+            console.log('You are player 2');
+    });
+    */
+}
+ServerTalk.prototype.sendGameData = function(data) {
+    this.socket.emit('game data', data);
+}
+ServerTalk.prototype.registerDataCallback = function (callback) {
+    this.dataCallback = callback;
+    this.socket.on('server game data', function (msg) {
+        this.dataCallback(msg);
+    });
+}
